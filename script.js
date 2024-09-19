9111bfd1f6e8f9b26761a51a56f874521a9de4bd
Vale <ortizsanchezvalentina07@gmail.com>
2024-09-18 21:04:36 -0600

    Variables

---
+++ script.js
@@ -0,0 +1,39 @@
+alert("ola desde el js");
+
+//18 09 2024 //ORTIZ SANCHEZ VALENTINA
+
+//comentario en una sola linea
+/*comentario
+entre
+varias 
+lineas*/
+
+//VARIABLES 
+/*
+Las variables guardan informacion que puedes reutilizar
+*/
+
+//Declaracion de Variables
+let nombre = "Valentina";
+const PI = 3.1416;
+var edad = 17;
+
+/*Tipos de datos:
+Cadena de texto (string): "Hola", "mundo"
+Numero (number): 5, 3, 14
+Booleano (boolean): true, false
+*/
+
+//Ejemplo
+let mensaje = 'hola mundo';
+let numero = 17;
+let Estudiante = true;
+
+//Operadores matematicos
+let a = 5;
+let b = 2;
+let suma = a+b; //7
+let resta = a-b; //3
+let multiplicacion = a*b; //10
+let division = a/b; //5/2=2.5
+
